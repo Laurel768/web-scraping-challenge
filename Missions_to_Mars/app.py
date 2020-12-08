fef87177b7b6b222a86c338472c78a94cc4c8ad8
Laurel768 <laurel.whitten@hotmail.com>
2020-12-07 22:13:17 -0500

    final files

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,28 +1,28 @@
 from flask import Flask, render_template, redirect
-from flask_pymongo import flask_pymongo
+from flask_pymongo import PyMongo
 import scrape_mars
 
 # Create an instance of Flask
 app = Flask(__name__)
 
 # Use PyMongo to establish Mongo connection
-mongo = PyMongo(app, url="mongodb://localhost:27017/mars")
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars")
 
 # Route to render index.html template using data from Mongo
 @app.route("/")
-def home():
+def index():
 
     # Find one record of data from the mongo database
-    destination_data = mongo.db.collection.find.one()
+    destination = mongo.db.collection.find_one()
    # Return template and data
-    return render_template("index.html", mars=destination_data)
+    return render_template("index.html", mars=destination)
 
 # Route that will trigger the scrape function
 @app.route("/scrape")
 def scrape():
 
     # Run the scrape function
-    mars_data = scrape_mars.scrape_info()
+    mars_data = scrape_mars.scrape()
 
     # Update the Mongo database using update and upsert=True
     mongo.db.collection.update({}, mars_data, upsert=True)
--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -4,80 +4,97 @@ from splinter import Browser
 import pandas as pd
 import pymongo
 
-url = 'https://mars.nasa.gov/news/'
+def init_browser():
+    executable_path = {'executable_path': './chromedriver.exe'}
+    browser = Browser('chrome', **executable_path, headless=False)
+    return browser
 
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
+def scrape():
+    browser = init_browser()
 
-title = soup.title.text
-print(title)
+    url = 'https://mars.nasa.gov/news/'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
 
-paragraphs = soup.find_all('p')
-for paragraph in paragraphs:
-    print(paragraph.text)
 
-executable_path = {'executable_path': './chromedriver.exe'}
-browser = Browser('chrome', **executable_path, headless=False)
+    title = soup.title.text
+    print(title)
 
-url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
-browser.visit(url)
+    paragraphs = soup.find_all('p')
+    for paragraph in paragraphs:
+        print(paragraph.text)
 
-for x in range(1):  
-    html = browser.html
+    mars_data = {
+        'Title' : title, 
+        'Paragraph' : paragraphs,
+        }    
+
+
+
+    url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+    browser.visit(url)
+
+    for x in range(1):  
+        html = browser.html
+        soup = BeautifulSoup(html, 'html.parser')
+        images = soup.find_all('a', class_='button fancybox')
+        
+    for image in images:
+        href = image["data-fancybox-href"]
+        print('https://www.jpl.nasa.gov' + href)
+        
+        featured_image_url = 'https://www.jpl.nasa.gov' + href
+
+        mars_data["featured_image"] = featured_image_url
+
+
+    url = 'https://space-facts.com/mars/'
+
+    tables = pd.read_html(url)
+    tables
+
+    type(tables)
+
+    df = tables[0]
+    df.columns = [' ',' ']
+    df.head()
+
+    html_table = df.to_html()
+    html_table
+
+    mars_data["Facts"] = html_table
+
+    conn = 'mongodb://localhost:27107'
+    client = pymongo.MongoClient(conn)
+    db = client.mars
+    collection = db.hemispheres
+
+    hemisphere_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    browser.visit(hemisphere_url)
+    html=browser.html
+
     soup = BeautifulSoup(html, 'html.parser')
-    images = soup.find_all('a', class_='button fancybox')
-    
-for image in images:
-    href = image["data-fancybox-href"]
-    print('https://www.jpl.nasa.gov' + href)
-    
-    featured_image_url = 'https://www.jpl.nasa.gov' + href
 
-    mars_data["featured"] = featured_image_url
+    hemisphere_image_urls = []
+
+    links = browser.find_by_css('a.product-item h3')
+
+    for l in range(len(links)):
+        hemisphere = {}
+        browser.find_by_css("a.product-item h3")[l].click()
+        sample_link=browser.links.find_by_text('Sample').first
+        hemisphere['url'] = sample_link['href']
+        hemisphere['title']=browser.find_by_css('h2.title').text
+        hemisphere_image_urls.append(hemisphere)
+        browser.back()
 
 
-url = 'https://space-facts.com/mars/'
 
-tables = pd.read_html(url)
-tables
+    # hemisphere_image_urls    
 
-type(tables)
+    mars_data["Hemispheres"] = hemisphere_image_urls
 
-df = tables[0]
-df.columns = [' ',' ']
-df.head()
+    browser.quit()
 
-html_table = df.to_html()
-html_table
+    return mars_data  
 
-mars_data["html_table"] = html_table
-
-conn = 'mongodb://localhost:27107'
-client = pymongo.MongoClient(conn)
-db = client.mars
-collection = db.hemispheres
-
-hemisphere_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
-browser.visit(hemisphere_url)
-html=browser.html
-
-soup = BeautifulSoup(html, 'html.parser')
-
-hemisphere_image_urls = []
-
-links = browser.find_by_css('a.product-item h3')
-
-for l in range(len(links)):
-    hemisphere = {}
-    browser.find_by_css("a.product-item h3")[l].click()
-    sample_link=browser.links.find_by_text('Sample').first
-    hemisphere['url'] = sample_link['href']
-    hemisphere['title']=browser.find_by_css('h2.title').text
-    hemisphere_image_urls.append(hemisphere)
-    browser.back()
-
-hemisphere_image_urls    
-
-browswer.quit()
-
-return mars_data
